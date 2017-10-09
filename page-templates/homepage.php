dd9ab5e650c750a305c358ef1376171bae427d7b
ozunaj <ozunaj@uthscsa.edu>
2017-10-09 08:57:23 -0500

    Update

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -4,8 +4,10 @@ Template Name: Homepage
 */
 get_header(); ?>
 <section class="intro-hero bleed-grey center white section grid-x">
-	<span class="fancy-header supa-size cell close">Jared Ozuna</span>
-	<h2 class="cell thin-header">web design & photography</h2>
-	<hr>
+	<div class="cell">
+		<span class="fancy-header supa-size close">Jared Ozuna</span>
+		<h2 class="thin-header">web design & photography</h2>
+		<hr>
+	</div>
 <section>
 <?php get_footer();
